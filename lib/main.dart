68a7e523389416ee0802f56b8bcd444c71c14182
omsnk <supanidang66@nu.ac.th>
2025-08-07 16:52:51 +0700

    add bottomLeft

--- lib/main.dart
+++ lib/main.dart
@@ -83,15 +83,11 @@ class MyHomePage extends StatelessWidget {
             bottom: 10.0,
             left: 10.0,
             child: Container(
-              padding: const EdgeInsets.all(4.0),
-              decoration: BoxDecoration(
-                color: Colors.red,
-                shape: BoxShape.circle, // ทำให้เป็นป้ายวงกลม
-              ),
-              child: const Icon(
-                Icons.notifications,
-                color: Colors.white,
-                size: 20,
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.black.withOpacity(0.5), // พื้นหลังกึ่งโปร่งใส
+              child: const Text(
+                'Bottom Left',
+                style: TextStyle(color: Colors.white),
               ),
             ),
           ),
